c1119fec85a9e58279bcd16ed4ea021f2125e2cf
adviti1 <113535364+adviti1@users.noreply.github.com>
2022-10-14 19:13:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// Game Constants & Variables
+
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('music/food.mp3');
-const moveSound = new Audio('music/move.mp3');
-const musicSound = new Audio('music/music.mp3');
 let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
@@ -36,18 +33,15 @@ function isCollide(snake) {
 }
 
 function gameEngine(){
-    // Part 1: Updating the snake array & Food
     if(isCollide(snakeArr)){
         gameOverSound.play();
-        musicSound.pause();
         inputDir =  {x: 0, y: 0}; 
         alert("Game Over. Press any key to play again!");
         snakeArr = [{x: 13, y: 15}];
-        musicSound.play();
         score = 0; 
     }
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
-        foodSound.play();
+       
         score += 1;
         if(score>hiscoreval){
             hiscoreval = score;
@@ -89,7 +83,6 @@ function gameEngine(){
 
 }
 
-musicSound.play();
 let hiscore = localStorage.getItem("hiscore");
 if(hiscore === null){
     hiscoreval = 0;
@@ -102,8 +95,7 @@ else{
 
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
-    inputDir = {x: 0, y: 1}
-    moveSound.play();
+    inputDir = {x: 0, y: 1};
     switch (e.key) {
         case "ArrowUp":
             console.log("ArrowUp");
